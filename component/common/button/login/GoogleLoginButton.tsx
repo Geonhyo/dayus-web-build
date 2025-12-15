0c5810cc46b614f398972a7b6f6a024f02a5d528
GeonhyoPark815 <rjsgy0815@gmail.com>
2025-12-15 21:52:32 +0900

    [Fix] Fix wrong url of google login

--- component/common/button/login/GoogleLoginButton.tsx
+++ component/common/button/login/GoogleLoginButton.tsx
@@ -16,7 +16,7 @@ export default function GoogleLoginButton({ className, redirect }: Props) {
   return (
     <Link
       className={`w-full h-14 flex flex-row items-center gap-4 bg-[#ffffff] px-6 rounded-2xl`}
-      href={`${process.env.NEXT_PUBLIC_API_URL}/auth/google?deviceName=${
+      href={`${process.env.NEXT_PUBLIC_API_URL}/web/auth/google?deviceName=${
         deviceInfo.deviceName
       }&osVersion=${deviceInfo.osVersion}&deviceId=${
         deviceInfo.deviceId
